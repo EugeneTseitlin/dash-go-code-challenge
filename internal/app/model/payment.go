2d1741b0cee8f8684452734e29add188c561f9c9
Eugene Tseitlin <e.a.tseitlin@gmal.com>
2021-07-30 17:54:27 +0300

    add mutual exclusion validation

--- internal/app/model/payment.go
+++ internal/app/model/payment.go
@@ -1,11 +1,11 @@
 package model
 
 type Payment struct {
-	ID string `json:"id" validate:"alphanum,max=256,required"`
-	Type string `json:"type" validate:"required"`
-	FromUserID string `json:"fromUserId" validate:"alphanum,max=256,required"`
-	ToMerchantID string `json:"toMerchantId" validate:"omitempty,alphanum,max=256"`
-	ToUserID string `json:"toUserId" validate:"omitempty,alphanum,max=256"`
-	Amount float64 `json:"amount" validate:"gt=0,required"`
-	CreatedAt string `json:"createdAt" validate:"iso8601,required"`
+	ID           string  `json:"id" validate:"alphanum,max=256,required"`
+	Type         string  `json:"type" validate:"required"`
+	FromUserID   string  `json:"fromUserId" validate:"alphanum,max=256,required"`
+	ToMerchantID string  `json:"toMerchantId" validate:"required_without=ToUserID,excluded_with=ToUserID,omitempty,alphanum,max=256,omitempty"`
+	ToUserID     string  `json:"toUserId" validate:"required_without=ToMerchantID,excluded_with=ToMerchantID,omitempty,alphanum,max=256"`
+	Amount       float64 `json:"amount" validate:"gt=0,required"`
+	CreatedAt    string  `json:"createdAt" validate:"iso8601,required"`
 }
